f97c7afaf45c84466e387c192315de0487a1907e
nothingKnow <mystery_cn@163.com>
2018-11-27 20:34:01 +0800

    Add files via upload

---
+++ src/main/webapp/static/images/icon_biz/_config.js
@@ -0,0 +1,9 @@
+//业务应用可覆盖此配置文件及此目录下的图片
+var imgCfg=[
+	{id:'16', name:'16*16', count:63, fileName:'16_?.png'},
+	{id:'24', name:'24*24', count:23, fileName:'24_?.png'},
+	{id:'desk', name:'桌面数据源', count:4, fileName:'desk_?.png', bgColor:'#CCC'},
+	{id:'shortcut', name:'办公快车道', count:9, fileName:'short_?.png'},
+	{id:'mobile', name:'移动办公', count:10, fileName:'mob_?.png', imageWidth:'35'}
+];
+
